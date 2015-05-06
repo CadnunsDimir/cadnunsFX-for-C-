238da896b079cd1a1ffbdc190260cf6fe9939083
unknown <tnascimento@NN-038.nuova2.com>
2015-05-06 12:13:24 -0300

    criando uma solução para o projeto CadnunsFx fazendo backup da primeira versão do Framework CadnunsFx

---
+++ CadnunsFx/Automation/PortaSerial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace CadnunsFx.Automation.Portas
+{
+    public class COM_Port
+    {
+        SerialPort portaSerial;
+
+        public COM_Port(String porta, int baudRate)
+        {
+            this.portaSerial.PortName = porta;
+            this.portaSerial.BaudRate = baudRate;
+        }
+
+        public void AbrirConexao()
+        {
+            if (!portaSerial.IsOpen)
+                portaSerial.Open();
+            else
+            MessageBox.Show("A porta "+portaSerial.PortName+" já está aberta");
+        }
+
+        public void FecharConexao()
+        {
+            if (portaSerial.IsOpen)
+            {
+                portaSerial.Close();
+            }
+        }
+
+        public static string[] RetornaPortasDisponiveis()
+        {
+            return SerialPort.GetPortNames();
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# CadnunsDimirFx for C Sharp
-Framework pessoal para desenvolvimento de apps em C#
-
